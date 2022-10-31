c70098e39aab0239a4f00d31cbbaf9ebfa3178b3
davidguillen2002 <david@LAPTOP-3KD5BQ0L>
2022-10-30 20:27:33 -0500

    Cambio

---
+++ RamosGuillenTrujilloProyectoPorgreso1/Models/Transporte.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RamosGuillenTrujilloProyectoPorgreso1.Models
+{
+    public class Transporte
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ingresa el tipo")]
+        public int Tipo { get; set; }
+
+        [Required(ErrorMessage = "Ingresa el modelo")]
+        public string Modelo { get; set; }
+
+        [Required(ErrorMessage = "Ingresa la mátricula")]
+        public string Matricula { get; set; }
+
+        [Required(ErrorMessage = "Ingresa la capacidad")]
+        [Range(10, 50, ErrorMessage = "Error. Ingrese un valor dentro los parámetros establecidos")]
+        public int Capacidad { get; set; }
+    }
+}
