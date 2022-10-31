68a2c53b46681f431191f47f732ec83c6e8f7630
Truji2002 <datrujillo2002@gmail.com>
2022-10-30 20:38:42 -0500

    Cambio en clase reserva

--- RamosGuillenTrujilloProyectoPorgreso1/Models/Conductor.cs
+++ RamosGuillenTrujilloProyectoPorgreso1/Models/Conductor.cs
@@ -5,7 +5,7 @@ namespace RamosGuillenTrujilloProyectoPorgreso1.Models
     public class Conductor
 
     {
-        
+
         public int IdConductor { get; set; }
 
         [Required(ErrorMessage = "Ingresa la cédula")]
@@ -13,27 +13,34 @@ namespace RamosGuillenTrujilloProyectoPorgreso1.Models
         public string? Cedula { get; set; }
 
         [Required(ErrorMessage = "Ingresa el nombre")]
-        [RegularExpression("[a-zA-Z] {1000}", ErrorMessage="Solo se permite ingresar letras.")]
-        
+        [RegularExpression("[a-zA-Z] {1000}", ErrorMessage = "Solo se permite ingresar letras.")]
+
         public string? Nombre { get; set; }
 
         [Required(ErrorMessage = "Ingresa el apellido")]
-        [RegularExpression("[a-zA-Z] {1000}", ErrorMessage="Solo se permite ingresar letras.")]
+        [RegularExpression("[a-zA-Z] {1000}", ErrorMessage = "Solo se permite ingresar letras.")]
         public string? Apellido { get; set; }
 
         [Required(ErrorMessage = "Ingresa el teléfono")]
-        [RegularExpression( "[0-9999999999] {10}" , ErrorMessage = "Solo se permite ingresar numeros.")]
+        [RegularExpression("[0-9999999999] {10}", ErrorMessage = "Solo se permite ingresar numeros.")]
         public string? NumeroTelefono { get; set; }
 
 
         [Required(ErrorMessage = "Ingresa el correo")]
-        [DataType(DataType.EmailAddress, ErrorMessage ="Ingrese un correo valido.")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Ingrese un correo valido.")]
         public string? Correo { get; set; }
 
         [Required(ErrorMessage = "Ingresa la fecha de nacimiento")]
-        [DataType(DataType.Date, ErrorMessage ="Ingrese una fecha valida")]
+        [DataType(DataType.Date, ErrorMessage = "Ingrese una fecha valida")]
         public DateOnly FechaNacimiento { get; set; }
 
+
+
+        public Conductor()
+        {
+
+        }  
+        
 
         public class ValidarCedula : ValidationAttribute
         {
