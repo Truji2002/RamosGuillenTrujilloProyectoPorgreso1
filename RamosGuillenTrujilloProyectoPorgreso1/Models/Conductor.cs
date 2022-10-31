802b99bbe91ad0472ee9af8f50b4125861bb1051
Truji2002 <datrujillo2002@gmail.com>
2022-10-30 20:05:16 -0500

    Clase Conductor

---
+++ RamosGuillenTrujilloProyectoPorgreso1/Models/Conductor.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RamosGuillenTrujilloProyectoPorgreso1.Models
+{
+    public class Conductor
+
+    {
+        
+        public int IdCliente { get; set; }
+
+        [Required(ErrorMessage = "Ingresa la cédula")]
+        [ValidarCedula]
+        public string? Cedula { get; set; }
+
+        [Required(ErrorMessage = "Ingresa el nombre")]
+        [RegularExpression("[a-zA-Z] {1000}", ErrorMessage="Solo se permite ingresar letras.")]
+        
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "Ingresa el apellido")]
+        [RegularExpression("[a-zA-Z] {1000}", ErrorMessage="Solo se permite ingresar letras.")]
+        public string? Apellido { get; set; }
+
+        [Required(ErrorMessage = "Ingresa el teléfono")]
+        [RegularExpression( "[0-9999999999] {10}" , ErrorMessage = "Solo se permite ingresar numeros.")]
+        public string? NumeroTelefono { get; set; }
+
+
+        [Required(ErrorMessage = "Ingresa el correo")]
+        [DataType(DataType.EmailAddress, ErrorMessage ="Ingrese un correo valido.")]
+        public string? Correo { get; set; }
+
+        [Required(ErrorMessage = "Ingresa la fecha de nacimiento")]
+        [DataType(DataType.Date, ErrorMessage ="Ingrese una fecha valida")]
+        public DateOnly FechaNacimiento { get; set; }
+
+
+        public class ValidarCedula : ValidationAttribute
+        {
+
+
+        }
+        
+
+
+
+
+
+        }
+}
