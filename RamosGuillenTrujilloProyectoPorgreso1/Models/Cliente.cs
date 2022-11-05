195eb19c9af55717b94c528b7e3704278e849534
Truji2002 <datrujillo2002@gmail.com>
2022-11-04 23:59:21 -0500

    ValidarCedulaImplementado

--- RamosGuillenTrujilloProyectoPorgreso1/Models/Cliente.cs
+++ RamosGuillenTrujilloProyectoPorgreso1/Models/Cliente.cs
@@ -47,4 +47,56 @@ namespace RamosGuillenTrujilloProyectoPorgreso1.Models
 
 
     }
+    public class ValidarCedula1 : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+
+        {
+            string valor1 = (string)value;
+            bool estado = false;
+            char[] valced = new char[13];
+            int provincia;
+            if (valor1.Length >= 10)
+            {
+                valced = valor1.Trim().ToCharArray();
+                provincia = int.Parse((valced[0].ToString() + valced[1].ToString()));
+                if (provincia > 0 && provincia < 25)
+                {
+                    estado = Verificar1(valced);
+                }
+
+            }
+            return estado;
+
+        }
+
+        public static bool Verificar1(char[] validarCedula)
+        {
+            int aux = 0, par = 0, impar = 0, verifi;
+            for (int i = 0; i < 9; i += 2)
+            {
+                aux = 2 * int.Parse(validarCedula[i].ToString());
+                if (aux > 9)
+                    aux -= 9;
+                par += aux;
+            }
+            for (int i = 1; i < 9; i += 2)
+            {
+                impar += int.Parse(validarCedula[i].ToString());
+            }
+
+            aux = par + impar;
+            if (aux % 10 != 0)
+            {
+                verifi = 10 - (aux % 10);
+            }
+            else
+                verifi = 0;
+            if (verifi == int.Parse(validarCedula[9].ToString()))
+                return true;
+            else
+                return false;
+        }
+
+    }
 }
