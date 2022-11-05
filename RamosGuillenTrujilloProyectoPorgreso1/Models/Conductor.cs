95029f92b99df77435f7ad3fa92fc67d5bde5b84
Truji2002 <datrujillo2002@gmail.com>
2022-11-04 23:31:33 -0500

    Validacion Cedula

--- RamosGuillenTrujilloProyectoPorgreso1/Models/Conductor.cs
+++ RamosGuillenTrujilloProyectoPorgreso1/Models/Conductor.cs
@@ -5,7 +5,7 @@ namespace RamosGuillenTrujilloProyectoPorgreso1.Models
     public class Conductor
 
     {
-        
+
         public int IdConductor { get; set; }
 
         [Display(Name = "Nombre")]
@@ -35,12 +35,12 @@ namespace RamosGuillenTrujilloProyectoPorgreso1.Models
 
         [Display(Name = "Correo electronico")]
         [Required(ErrorMessage = "Ingresa el correo")]
-        [DataType(DataType.EmailAddress, ErrorMessage ="Ingrese un correo valido.")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Ingrese un correo valido.")]
         public string? Correo { get; set; }
 
         [Display(Name = "Fecha de nacimiento")]
         [Required(ErrorMessage = "Ingresa la fecha de nacimiento")]
-        [DataType(DataType.Date, ErrorMessage ="Ingrese una fecha valida")]
+        [DataType(DataType.Date, ErrorMessage = "Ingrese una fecha valida")]
         public DateTime FechaNacimiento { get; set; }
 
         [Display(Name = "Fecha de emisión de la licencia")]
@@ -55,21 +55,65 @@ namespace RamosGuillenTrujilloProyectoPorgreso1.Models
 
         [Display(Name = "Contraseña")]
         [Required(ErrorMessage = "Ingresa la contraseña")]
-        [DataType(DataType.Password, ErrorMessage ="Ingrese una contraseña valida")]
+        [DataType(DataType.Password, ErrorMessage = "Ingrese una contraseña valida")]
         public string? Contraseña { get; set; }
 
-
-
+    }
         public class ValidarCedula : ValidationAttribute
         {
+            public override bool IsValid(object? value)
 
+            {
+                string valor1 = (string)value;
+                bool estado = false;
+                char[] valced = new char[13];
+                int provincia;
+                if (valor1.Length >= 10)
+                {
+                    valced = valor1.Trim().ToCharArray();
+                    provincia = int.Parse((valced[0].ToString() + valced[1].ToString()));
+                    if (provincia > 0 && provincia < 25)
+                    {
+                        estado = Verificar(valced);
+
+
+                    }
+
+
+                }
+                return estado;
+
+            }
+
+            public static bool Verificar(char[] validarCedula)
+            {
+            int aux = 0, par = 0, impar = 0, verifi;
+            for (int i = 0; i < 9; i += 2)
+            {
+                aux = 2 * int.Parse(validarCedula[i].ToString());
+                if (aux > 9)
+                    aux -= 9;
+                par += aux;
+            }
+            for (int i = 1; i < 9; i += 2)
+            {
+                impar += int.Parse(validarCedula[i].ToString());
+            }
+
+            aux = par + impar;
+            if (aux % 10 != 0)
+            {
+                verifi = 10 - (aux % 10);
+            }
+            else
+                verifi = 0;
+            if (verifi == int.Parse(validarCedula[9].ToString()))
+                return true;
+            else
+                return false;
+        }
 
         }
+
         
-
-
-
-
-
-        }
 }
