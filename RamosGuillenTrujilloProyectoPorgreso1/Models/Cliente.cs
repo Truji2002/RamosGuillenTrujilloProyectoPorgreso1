c5259046ff67c207d6e4c7be8c9901646291fdfb
Xavi122323 <xavier.ramos@udla.edu.ec>
2022-11-04 21:57:48 -0500

    Actualización cliente

--- RamosGuillenTrujilloProyectoPorgreso1/Models/Cliente.cs
+++ RamosGuillenTrujilloProyectoPorgreso1/Models/Cliente.cs
@@ -5,15 +5,40 @@ namespace RamosGuillenTrujilloProyectoPorgreso1.Models
     public class Cliente
 
     {
+        [Key]
         public int IdCliente { get; set; }
-        [Required(ErrorMessage = "Debe ingresar un nombre")]
-        public string NombreCompleto { get; set; }
-        public string Cedula { get; set; }
-        [MaxLength(10)]
-        public string Telefono { get; set; }
-        [DataType(DataType.EmailAddress, ErrorMessage = "Correo no valido")]
+
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "Ingrese su nombre")]
+        [RegularExpression("^[a-zA-Z]*${1,100}", ErrorMessage = "Solo puede ingresar letras")]
+        public string? Nombre { get; set; }
+
+        [Display(Name = "Apellido")]
+        [Required(ErrorMessage = "Ingrese su apellido")]
+        [RegularExpression("^[a-zA-Z]*${1,100}", ErrorMessage = "Solo puede ingresar letras")]
+        public string? Apellido { get; set; }
+
+        [Display(Name = "Cedula")]
+        [Required(ErrorMessage = "Ingrese su cedula")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Solo puede ingresar numeros")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Su cedula debe tener 10 números")]
+        public string? Cedula { get; set; }
+
+        [Display(Name = "Celular")]
+        [Required(ErrorMessage = "Ingrese su celular")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Solo puede ingresar numeros")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Su celular debe tener 10 números")]
+        public string? Telefono { get; set; }
+
+        [Display(Name = "Correo electronico")]
+        [Required(ErrorMessage = "Ingrese su correo electrónico")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Ingrese un correo valido")]
         public string? CorreoElectronico { get; set; }
-        public DateOnly FechaNacimiento { get; set; }
+
+        [Display(Name = "Fecha de nacimiento")]
+        [Required(ErrorMessage = "Ingrese su fecha de nacimiento")]
+        [DataType(DataType.DateTime, ErrorMessage = "Ingrese una fecha valida")]
+        public DateTime FechaNacimiento { get; set; }
 
     }
 }
