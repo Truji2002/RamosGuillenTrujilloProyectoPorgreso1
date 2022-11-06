fe27167665a3dff3bb9016e409de480df13422fa
Truji2002 <datrujillo2002@gmail.com>
2022-11-06 12:34:19 -0500

    Base de datos

---
+++ RamosGuillenTrujilloProyectoPorgreso1/Migrations/20221106173353_Cambio1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RamosGuillenTrujilloProyectoPorgreso1.Migrations
+{
+    public partial class Cambio1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reserva",
+                columns: table => new
+                {
+                    IdReserva = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaPartida = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PuntoPartida = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CantPersonas = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reserva", x => x.IdReserva);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ruta",
+                columns: table => new
+                {
+                    IdRuta = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaEmisionLicencia = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaVencimientoLicencia = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PuntoLlegada = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precio = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ruta", x => x.IdRuta);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transporte",
+                columns: table => new
+                {
+                    IdTransporte = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Matricula = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Capacidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transporte", x => x.IdTransporte);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    IdCliente = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cedula = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    CorreoElectronico = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Contraseña = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    idReserva = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.IdCliente);
+                    table.ForeignKey(
+                        name: "FK_Cliente_Reserva_IdCliente",
+                        column: x => x.IdCliente,
+                        principalTable: "Reserva",
+                        principalColumn: "IdReserva",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Conductor",
+                columns: table => new
+                {
+                    IdConductor = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cedula = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    NumeroTelefono = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaEmisionLicencia = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaVencimientoLicencia = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Contraseña = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdRuta = table.Column<int>(type: "int", nullable: false),
+                    IdTransporte = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conductor", x => x.IdConductor);
+                    table.ForeignKey(
+                        name: "FK_Conductor_Ruta_IdConductor",
+                        column: x => x.IdConductor,
+                        principalTable: "Ruta",
+                        principalColumn: "IdRuta",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Conductor_Transporte_IdConductor",
+                        column: x => x.IdConductor,
+                        principalTable: "Transporte",
+                        principalColumn: "IdTransporte",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+
+            migrationBuilder.DropTable(
+                name: "Conductor");
+
+            migrationBuilder.DropTable(
+                name: "Reserva");
+
+            migrationBuilder.DropTable(
+                name: "Ruta");
+
+            migrationBuilder.DropTable(
+                name: "Transporte");
+        }
+    }
+}
