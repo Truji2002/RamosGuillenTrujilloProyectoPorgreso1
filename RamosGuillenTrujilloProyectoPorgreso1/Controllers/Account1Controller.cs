0e441203bd66d421d4b44bc52219922a03516416
davidguillen2002 <david@LAPTOP-3KD5BQ0L>
2022-11-06 15:28:03 -0500

    Registro

---
+++ RamosGuillenTrujilloProyectoPorgreso1/Controllers/Account1Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RamosGuillenTrujilloProyectoPorgreso1.Models;
+
+namespace RamosGuillenTrujilloProyectoPorgreso1.Controllers
+{
+    public class Account1Controller : Controller
+    {
+        [Route("registrocliente")]
+        public IActionResult RegistrarseCliente()
+        {
+            return View();
+        }
+
+        [Route("registrocliente")]
+        [HttpPost]
+        public IActionResult RegistrarseCliente(Conductor conductor)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
---
+++ RamosGuillenTrujilloProyectoPorgreso1/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using RamosGuillenTrujilloProyectoPorgreso1.Models;
+using RamosGuillenTrujilloProyectoPorgreso1.Repository;
+
+namespace RamosGuillenTrujilloProyectoPorgreso1.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("registroconductor")]
+        public IActionResult RegistrarseConductor()
+        {
+            return View();
+        }
+
+        [Route("registroconductor")]
+        [HttpPost]
+        public IActionResult RegistrarseConductor(Conductor conductor)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
