759946cc57e1cb6b19b0ed7fa50b86ec876cdfc2
davidguillen2002 <david@LAPTOP-3KD5BQ0L>
2022-11-06 15:54:22 -0500

    Login conductor

---
+++ RamosGuillenTrujilloProyectoPorgreso1/Models/SignInModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.Design;
+using System.Xml.Linq;
+
+namespace RamosGuillenTrujilloProyectoPorgreso1.Models
+{
+    public class SignInModel
+    {
+        [ForeignKey("Conductor")]
+        [Display(Name = "Correo electronico")]
+        [Required(ErrorMessage = "Ingrese su correo electrónico")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Ingrese un correo valido")]
+        public string? CorreoElectronico { get; set; }
+
+        [ForeignKey("Conductor")]
+        [Required(ErrorMessage = "Ingresa la contraseña")]
+        [DataType(DataType.Password, ErrorMessage = "Ingrese una contraseña valida")]
+        public string? Contraseña { get; set; }
+    }
+}
