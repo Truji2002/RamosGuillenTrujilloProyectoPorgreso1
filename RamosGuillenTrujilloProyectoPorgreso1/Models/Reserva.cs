20f263c20bbb3fba6d53acf28a751a4e979cf2e6
Truji2002 <datrujillo2002@gmail.com>
2022-11-06 12:06:11 -0500

    Cambio R,T,R

--- RamosGuillenTrujilloProyectoPorgreso1/Models/Reserva.cs
+++ RamosGuillenTrujilloProyectoPorgreso1/Models/Reserva.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
 
 namespace RamosGuillenTrujilloProyectoPorgreso1.Models
 {
@@ -7,15 +9,41 @@ namespace RamosGuillenTrujilloProyectoPorgreso1.Models
         
        
     {
-       /* 
-        public int IdReserva { get; set; }
-
+       
         
 
-        //[ForeignKey] 
+
+
+        [Key]
+        public int IdReserva { get; set; }
 
 
 
-        */
+        [ForeignKey("IdCliente")]
+        //public int IdCliente   { get; set; }
+
+
+
+        [Display(Name = "Fecha de partida")]
+        [Required(ErrorMessage = "Ingresa la fecha de partida del viaje")]
+        [DataType(DataType.Date, ErrorMessage = "Ingrese una fecha valida")]
+        public DateTime FechaPartida { get; set; }
+
+
+        [Display(Name = "Punto de partida (Direccion)")]
+        [Required(ErrorMessage = "Debe ingresar el punto de partida")]
+
+        public string? PuntoPartida { get; set; }
+
+
+        [Required(ErrorMessage = "Ingresa la cantidad de personas que viajan")]
+        [Display(Name = "Cantidad personas que viajan")]
+        [Range(10, 50, ErrorMessage = "Error. Ingrese un valor dentro los parámetros establecidos")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Solo puede ingresar numeros")]
+        public int? CantPersonas { get; set; }
+
+
+
+
     }
 }
--- RamosGuillenTrujilloProyectoPorgreso1/Models/Ruta.cs
+++ RamosGuillenTrujilloProyectoPorgreso1/Models/Ruta.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RamosGuillenTrujilloProyectoPorgreso1.Models
 {
@@ -7,10 +8,19 @@ namespace RamosGuillenTrujilloProyectoPorgreso1.Models
         [Key]
         public int IdRuta { get; set; }
 
-        [Display(Name = "Punto de partida")]
-        [Required(ErrorMessage = "Debe ingresar el punto de partida")]
-        [RegularExpression("^[a-zA-Z]*${1,100}", ErrorMessage = "Solo puede ingresar letras")]
-        public string? PuntoPartida { get; set; }
+        [ForeignKey("IdCliente")]
+
+        [Display(Name = "Fecha de emisión de la licencia")]
+        [Required(ErrorMessage = "Ingresa la fecha de emision de la licencia")]
+        [DataType(DataType.Date, ErrorMessage = "Ingrese una fecha valida")]
+        public DateTime FechaEmisionLicencia { get; set; }
+
+        [Display(Name = "Fecha de vencimiento de la licencia")]
+        [Required(ErrorMessage = "Ingresa la fecha de vencimiento de la licencia")]
+        [DataType(DataType.Date, ErrorMessage = "Ingrese una fecha valida")]
+        public DateTime FechaVencimientoLicencia { get; set; }
+
+
 
         [Display(Name = "Punto de llegada")]
         [Required(ErrorMessage = "Debe ingresar el punto de llegada")]
@@ -22,33 +32,5 @@ namespace RamosGuillenTrujilloProyectoPorgreso1.Models
         [RegularExpression("^[0.0-9.9]*$", ErrorMessage = "Solo puede ingresar numeros")]
         [Range(50.0, 500, ErrorMessage = "Error. Ingrese un valor dentro los parámetros establecidos")]
         public double Precio { get; set; }
-
-        [Display(Name = "Fecha y hora de partida")]
-
-        [Required(ErrorMessage = "Ingresa la fecha de partida")]
-        [DataType(DataType.Date, ErrorMessage = "Ingrese una fecha valida")]
-        public DateTime FechaPartida { get; set; }
-
-        //public DateTime HoraAproximadaLlegada { get; set; }
-
-        [Required(ErrorMessage = "Ingresa el tipo")]
-        [Display(Name = "Tipo de transporte")]
-        [RegularExpression("^[a-zA-Z]*${1,100}", ErrorMessage = "Solo puede ingresar letras")]
-        public string? Tipo { get; set; }
-
-        [Required(ErrorMessage = "Ingresa el modelo")]
-        [Display(Name = "Modelo del transporte")]
-        [RegularExpression("^[a-zA-Z]*${1,100}", ErrorMessage = "Solo puede ingresar letras")]
-        public string? Modelo { get; set; }
-
-        [Required(ErrorMessage = "Ingresa la mátricula")]
-        [Display(Name = "Mátricula del transporte ")]
-        [RegularExpression("^[a-zA-Z]*${1,100}", ErrorMessage = "Solo puede ingresar letras")]
-        public string? Matricula { get; set; }
-
-        [Required(ErrorMessage = "Ingresa la capacidad")]
-        [Display(Name = "Capacidad de personas del transporte")]
-        [Range(10, 50, ErrorMessage = "Error. Ingrese un valor dentro los parámetros establecidos")]
-        public int? Capacidad { get; set; }
     }
 }
