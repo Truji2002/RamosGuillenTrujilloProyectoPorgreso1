d1b9adb0796b565b986cc8eab75848c75c5050f6
davidguillen2002 <david@LAPTOP-3KD5BQ0L>
2022-11-06 19:22:55 -0500

    .

---
+++ RamosGuillenTrujilloProyectoPorgreso1/Areas/Identity/Data/RamosGuillenTrujilloProyectoPorgreso1Context1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RamosGuillenTrujilloProyectoPorgreso1.Areas.Identity.Data;
+
+namespace RamosGuillenTrujilloProyectoPorgreso1.Data;
+
+public class RamosGuillenTrujilloProyectoPorgreso1Context1 : IdentityDbContext<ApplicationUser>
+{
+    public RamosGuillenTrujilloProyectoPorgreso1Context1(DbContextOptions<RamosGuillenTrujilloProyectoPorgreso1Context1> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
