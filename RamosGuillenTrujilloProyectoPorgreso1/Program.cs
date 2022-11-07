d0ac4396445dc6de64220c9d2cc3a6f33a6bd2e9
davidguillen2002 <david@LAPTOP-3KD5BQ0L>
2022-11-06 20:13:59 -0500

    .

--- RamosGuillenTrujilloProyectoPorgreso1/Program.cs
+++ RamosGuillenTrujilloProyectoPorgreso1/Program.cs
@@ -7,7 +7,7 @@ using RamosGuillenTrujilloProyectoPorgreso1.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<RamosGuillenTrujilloProyectoPorgreso1Context5>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("RamosGuillenTrujilloProyectoPorgreso1Context5Connection") ?? throw new InvalidOperationException("Connection string 'RamosGuillenTrujilloProyectoPorgreso1Context' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RamosGuillenTrujilloProyectoPorgreso1Context5Connection") ?? throw new InvalidOperationException("Connection string 'RamosGuillenTrujilloProyectoPorgreso1Context5' not found.")));
 
 builder.Services.AddDefaultIdentity<ApplicationUser5>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<RamosGuillenTrujilloProyectoPorgreso1Context5>();
