8aeeabe8dc5b5d3c343765853ed6f5d597e2b739
davidguillen2002 <david@LAPTOP-3KD5BQ0L>
2022-11-06 19:37:29 -0500

    .

--- RamosGuillenTrujilloProyectoPorgreso1/Program.cs
+++ RamosGuillenTrujilloProyectoPorgreso1/Program.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using RamosGuillenTrujilloProyectoPorgreso1.Data;
+using Microsoft.AspNetCore.Identity;
+using RamosGuillenTrujilloProyectoPorgreso1.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<RamosGuillenTrujilloProyectoPorgreso1Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("RamosGuillenTrujilloProyectoPorgreso1Context") ?? throw new InvalidOperationException("Connection string 'RamosGuillenTrujilloProyectoPorgreso1Context' not found.")));
+
+builder.Services.AddDefaultIdentity<ApplicationUser5>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<RamosGuillenTrujilloProyectoPorgreso1Context5>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -22,6 +28,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
