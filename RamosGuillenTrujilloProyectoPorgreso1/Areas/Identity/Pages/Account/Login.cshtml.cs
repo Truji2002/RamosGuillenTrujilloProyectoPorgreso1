e991954ec904ccf7bb95c746e6ae60c6b96dba0c
davidguillen2002 <david@LAPTOP-3KD5BQ0L>
2022-11-06 22:26:58 -0500

    .

--- RamosGuillenTrujilloProyectoPorgreso1/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ RamosGuillenTrujilloProyectoPorgreso1/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -81,7 +81,7 @@ namespace RamosGuillenTrujilloProyectoPorgreso1.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Recuerdame ?")]
+            [Display(Name = "Remember me?")]
             public bool RememberMe { get; set; }
         }
 
