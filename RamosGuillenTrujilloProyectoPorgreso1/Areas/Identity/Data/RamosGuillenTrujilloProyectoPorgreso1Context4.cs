154709a10815dcabe8b1a0a33d692d6bbbb60c8e
davidguillen2002 <david@LAPTOP-3KD5BQ0L>
2022-11-06 19:28:45 -0500

    .

---
+++ RamosGuillenTrujilloProyectoPorgreso1/Areas/Identity/Data/RamosGuillenTrujilloProyectoPorgreso1Context4.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RamosGuillenTrujilloProyectoPorgreso1.Areas.Identity.Data;
+
+namespace RamosGuillenTrujilloProyectoPorgreso1.Data;
+
+public class RamosGuillenTrujilloProyectoPorgreso1Context4 : IdentityDbContext<ApplicationUser>
+{
+    public RamosGuillenTrujilloProyectoPorgreso1Context4(DbContextOptions<RamosGuillenTrujilloProyectoPorgreso1Context4> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- RamosGuillenTrujilloProyectoPorgreso1/Program.cs
+++ RamosGuillenTrujilloProyectoPorgreso1/Program.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using RamosGuillenTrujilloProyectoPorgreso1.Data;
+using Microsoft.AspNetCore.Identity;
+using RamosGuillenTrujilloProyectoPorgreso1.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<RamosGuillenTrujilloProyectoPorgreso1Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("RamosGuillenTrujilloProyectoPorgreso1Context") ?? throw new InvalidOperationException("Connection string 'RamosGuillenTrujilloProyectoPorgreso1Context' not found.")));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<RamosGuillenTrujilloProyectoPorgreso1Context4>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -22,6 +27,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
