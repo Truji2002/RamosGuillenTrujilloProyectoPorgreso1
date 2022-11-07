c82e5e84c4078c0d4806f17135ed34d71530d7e8
Truji2002 <datrujillo2002@gmail.com>
2022-11-06 22:37:52 -0500

    .

---
+++ RamosGuillenTrujilloProyectoPorgreso1/Migrations/20221107033602_MyMigration5.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RamosGuillenTrujilloProyectoPorgreso1.Migrations
+{
+    public partial class MyMigration5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IdPromociones",
+                table: "Promocion",
+                newName: "IdPromocion");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IdPromocion",
+                table: "Promocion",
+                newName: "IdPromociones");
+        }
+    }
+}
